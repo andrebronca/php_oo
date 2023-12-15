ff07e9219266d2ddaad439fdaa3da35ee9a7de91
André R. B <bronca.andre@gmail.com>
2023-12-15 16:28:11 -0300

    definindo atributo e método private.

--- Funcionario.php
+++ Funcionario.php
@@ -1,15 +1,17 @@
 <?php
 
-//exemplo simples
+//exemplo com atributo e método private
 class Funcionario {
     public string $nome;
     public float $salario;
+    private string $salarioConvertido;  
 
     public function verSalario():string {
         return "Funcionário: {$this->nome}, salário: R$ {$this->converterSalario()} <hr>";
     }
 
     private function converterSalario():string {
-        return number_format($this->salario, '2', ',', '.');
+        $this->salarioConvertido = number_format($this->salario, '2', ',', '.');
+        return $this->salarioConvertido;
     }
 }
